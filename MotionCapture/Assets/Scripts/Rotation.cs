1be1c07bc5194a5cc6209d70e7b1229645552b7e
Tito <htp.tito@gmail.com>
2018-01-21 21:39:18 -0800

    TCP Communication class

--- MotionCapture/Assets/Scripts/Rotation.cs
+++ MotionCapture/Assets/Scripts/Rotation.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour {
 
+	private Vector3 result;
+
 	// Use this for initialization
 	void Start () {
-		
+		result = Mathf.Cos (Mathf.PI/4) * Vector3.right + Mathf.Sin (Mathf.PI/4) * Vector3.forward;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine (Vector3.zero, Vector3.right);
+		Gizmos.DrawLine (Vector3.zero, result);
 	}
 }
---
+++ MotionCapture/Assets/Scripts/TcpConnectedIMU.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System;
+
+public class TcpConnectedIMU
+{
+	/// <summary>
+	/// Conexión a IMU (cliente)
+	/// </summary>
+	readonly TcpClient connection;
+
+	readonly byte[] readBuffer = new byte[5000];
+	NetworkStream stream { get{ return connection.GetStream(); } }
+
+	public TcpConnectedIMU(TcpClient tcpClient)
+	{
+		connection = tcpClient;
+		connection.NoDelay = true; // deshabilitar caché
+		stream.BeginRead(readBuffer, 0, readBuffer.Length, OnRead, null);
+	}
+
+	internal void Close()
+	{
+		connection.Close();
+	}
+
+	void OnRead(IAsyncResult ar)
+	{
+		int length = stream.EndRead(ar);
+		if(length <= 0)
+		{ // Connection closed
+			//TCPChat.instance.OnDisconnect(this);
+			return;
+		}
+
+		string newMessage = System.Text.Encoding.UTF8.GetString(readBuffer, 0, length);
+		//TcpIMUCommunication.messageToDisplay += newMessage + Environment.NewLine;
+		Debug.Log(newMessage);
+//		if(TCPChat.instance.isServer)
+//		{
+//			TCPChat.BroadcastChatMessage(newMessage);
+//		}
+
+		stream.BeginRead(readBuffer, 0, readBuffer.Length, OnRead, null);
+	}
+
+	internal void EndConnect(IAsyncResult ar)
+	{
+		connection.EndConnect(ar);
+		stream.BeginRead(readBuffer, 0, readBuffer.Length, OnRead, null);
+	}
+}
