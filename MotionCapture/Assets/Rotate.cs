13a8f1db6c47aa3b37958f6a6ae41449a9e5d59f
Trujillo, Humberto <humberto.trujillo@thermofisher.com>
2018-06-06 19:39:05 -0700

    Rotation right hand vs left hand

---
+++ MotionCapture/Assets/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour 
+{
+	public float speed = 2.0f;
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * Time.deltaTime * speed);		
+	}
+}
