86c51db3811d23fa4d7bf593e45af10e430a7cd6
Trujillo, Humberto <humberto.trujillo@thermofisher.com>
2018-06-18 17:08:51 -0700

    Transform Recorder.

---
+++ MotionCapture/Assets/Scripts/RecordTransformHierarchy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor.Experimental.Animations;
+
+public class RecordTransformHierarchy : MonoBehaviour
+{
+    public AnimationClip clip;
+    public bool record = false;
+
+    private GameObjectRecorder m_Recorder;
+
+    void Start()
+    {
+        m_Recorder = new GameObjectRecorder(gameObject);
+
+        m_Recorder.BindComponent<Transform>(gameObject, true);
+    }
+
+    void LateUpdate()
+    {
+        if (clip == null)
+            return;
+
+        if (record)
+        {
+            m_Recorder.TakeSnapshot(Time.deltaTime);
+        }
+        else if (m_Recorder.isRecording)
+        {
+            m_Recorder.SaveToClip(clip);
+            m_Recorder.ResetRecording();
+        }
+    }
+}
